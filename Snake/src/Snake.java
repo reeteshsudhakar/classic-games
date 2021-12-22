06e2174415694457d1543d53e50cb20433239d0c
Reetesh Sudhakar <reesud6187@gmail.com>
2021-12-22 00:57:52 -0500

    Add files via upload

---
+++ Snake/src/Snake.java
@@ -0,0 +1,100 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Snake {
+
+    List<Point> snakePoints;
+    int xDir, yDir;
+    boolean isMoving, elongate;
+    final int STARTSIZE = 20, STARTX = 150, STARTY = 150;
+
+    public Snake() {
+        snakePoints = new ArrayList<Point>();
+        xDir = 0;
+        yDir = 0;
+        isMoving = false;
+        elongate = false;
+        snakePoints.add(new Point(STARTX, STARTY));
+
+        for (int i = 1; i < STARTSIZE; i++) {
+            snakePoints.add(new Point(STARTX - i * 4, STARTY));
+        }
+
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.WHITE);
+
+        for (Point p : snakePoints) {
+            g.fillRect(p.getX(), p.getY(), 4, 4);
+        }
+
+    }
+
+    public void move() {
+        if (isMoving) {
+            Point temp = snakePoints.get(0);
+            Point last = snakePoints.get(snakePoints.size() - 1);
+            Point newStart = new Point(temp.getX() + xDir * 4, temp.getY() + yDir * 4);
+
+            for (int i = snakePoints.size() - 1; i >= 1; i--) {
+                snakePoints.set(i, snakePoints.get(i - 1));
+            }
+            snakePoints.set(0, newStart);
+
+            if (elongate) {
+                snakePoints.add(last);
+                elongate = false;
+            }
+        }
+    }
+
+    public boolean snakeCollision() {
+        int x = this.getX();
+        int y = this.getY();
+
+        for (int i = 1; i < snakePoints.size(); i++) {
+            if (x == snakePoints.get(i).getX() && y == snakePoints.get(i).getY()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getXDir() {
+        return xDir;
+    }
+
+    public int getYDir() {
+        return yDir;
+    }
+
+    public void setXDir(int x) {
+        xDir = x;
+    }
+
+    public void setYDir(int y) {
+        yDir = y;
+    }
+
+    public int getX() {
+        return snakePoints.get(0).getX();
+    }
+
+    public int getY() {
+        return snakePoints.get(0).getY();
+    }
+
+    public boolean isMoving() {
+        return isMoving;
+    }
+
+    public boolean setMoving(boolean b) {
+        return isMoving = b;
+    }
+
+    public void setElongate(boolean b){
+        elongate = b;
+    }
+}
---
+++ Snake/src/Token.java
@@ -0,0 +1,42 @@
+import java.awt.*;
+
+public class Token {
+
+    private int x, y, score;
+    private Snake snake;
+
+    public Token(Snake snake) {
+        x = (int) (Math.random() * 395);
+        y = (int) (Math.random() * 395);
+        this.snake = snake;
+    }
+
+    public void changePosition() {
+        x = (int) (Math.random() * 395);
+        y = (int) (Math.random() * 395);
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.GREEN);
+        g.fillRect(x, y, 6,6);
+    }
+
+    public boolean snakeCollision() {
+        int snakeX = snake.getX() + 2;
+        int snakeY = snake.getY() + 2;
+
+        if (snakeX >= x - 1 && snakeX <= x + 7) {
+            if (snakeY >= y - 1 && snakeY <= y + 7) {
+                changePosition();
+                score++;
+                snake.setElongate(true);
+                return true;
+            }
+        }
+        return false;
+    }
+}
